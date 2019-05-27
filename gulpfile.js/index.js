fdf2635152bf4f794132383e22f8cbcb05d473d0
郑城城 <zhengcc@xbzoom.com>
2019-05-27 17:46:07 +0800

    +

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,5 +1,9 @@
+/**
+ * 注意task中,src和dest中的目录,都是以gulpfile.js为相对目录的,不是以当前任务的实际问价目录
+ */
 const { series } = require('gulp')
 
-var { copy } = require('./copy.js')
+// 导入task
+const { copy } = require('./copy.js')
 
 exports.dev = series(copy)
